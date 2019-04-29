1cf6fdd444a622e5bb8a036ddd8cfd6e9bf7c2b1
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-04-29 23:39:25 +0545

    Create common.mk

---
+++ common.mk
@@ -0,0 +1,11 @@
+# Audio
+PRODUCT_PACKAGES += \
+    audio_amplifier.msm8916 \
+    audio.a2dp.default \
+    audio.primary.msm8916 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing
