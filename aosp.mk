8f770af91a7c0c6d50da301abbce564d43c2d1c7
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-01 18:11:04 +0545

    Create aosp.mk

---
+++ aosp.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy A5
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit some common Pixel Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_ARCH := arm
+TARGET_DENSITY := hdpi
+TARGET_GAPPS_ARCH := arm
