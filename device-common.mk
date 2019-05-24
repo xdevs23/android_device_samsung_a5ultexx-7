d004b50849349bfe356791fa91c45a65e66fb3de
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-24 22:56:16 +0545

    x

--- device-common.mk
+++ device-common.mk
@@ -40,6 +40,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     InputDisabler
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
 
