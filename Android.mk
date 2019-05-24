7551f16e7d753c08fd9a25200c2e27e438e72727
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-24 08:54:37 +0545

    x

--- Android.mk
+++ Android.mk
@@ -4,13 +4,4 @@ ifneq ($(filter a5ltexx a5lte a5ultexx a5ulte a53g a53gxx, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := InputDisabler
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
 endif
