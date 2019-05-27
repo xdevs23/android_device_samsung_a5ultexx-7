984356f02837707d4a876efbc7e5317b60fd39d4
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-27 10:47:34 +0545

    x

--- product/qcom-audio.mk
+++
@@ -1,7 +0,0 @@
-# Audio configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
-
-# Platform info config
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
