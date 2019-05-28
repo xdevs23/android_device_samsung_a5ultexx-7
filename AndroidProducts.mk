fe6db5a00974c35c3254e0979f50e92be1815c6f
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-28 19:40:00 +0545

    x

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := device/samsung/a5ultexx
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/rr_a5ultexx.mk \
+	$(LOCAL_DIR)/havoc_a5ultexx.mk \
+	$(LOCAL_DIR)/lineage_a5ultexx.mk \
+	$(LOCAL_DIR)/aosp_a5ultexx.mk
