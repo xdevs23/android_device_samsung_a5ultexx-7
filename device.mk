41db2620b94f2fe37a4e95eea22e14d4da403107
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-31 20:00:54 +0545

    x

--- device.mk
+++
@@ -1,7 +0,0 @@
-#Inherit from vendor
-$(call inherit-product-if-exists, vendor/samsung/a5ultexx/a5ultexx-vendor-blobs.mk)
-
-# Inherit from common
-$(call inherit-product, device/samsung/a5ultexx/common.mk)
-
-LOCAL_PATH := device/samsung/a5ultexx
